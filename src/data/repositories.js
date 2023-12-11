82a716e1625a99715ef173d59a5337ac4b4a05f1
bitster22 <57819533+bitster22@users.noreply.github.com>
2023-12-11 10:35:16 -0300

    feat: Data adicionada

---
+++ src/data/repositories.js
@@ -0,0 +1,89 @@
+export const repositories_back = [
+    {
+        name: "Pet Kare",
+        language: "Python",
+        description: "API para cuidado de pets.",
+        github_link: "https://github.com/bitster22/m5-pet-kare-alvaro",
+        vercel_link: ""
+    },
+    {
+        name: "Kenzie Buster",
+        language: "Python",
+        description: "API para gestão de filmes.",
+        github_link: "https://github.com/bitster22/m5-kenzie-buster-alvaro",
+        vercel_link: ""
+    },
+    {
+        name: "BandKamp Generic View",
+        language: "Python",
+        description: "API de cadastro de músicas.",
+        github_link: "https://github.com/bitster22/m5-bandkamp-generic-view-alvaro",
+        vercel_link: ""
+    },
+    {
+        name: "KImóveis",
+        language: "Node JS",
+        description: "Api de gerenciamento de imobiliárias.",
+        github_link: "https://github.com/bitster22/kimoveis",
+        vercel_link: ""
+    },
+    {
+        name: "User e Courses",
+        language: "Node JS",
+        description: "API de gerenciamento de cursos.",
+        github_link: "https://github.com/bitster22/user-courses",
+        vercel_link: ""
+    },
+    {
+        name: "KenzieVelopers",
+        language: "Node JS",
+        description: "API de gerenciamento de desenvolvedores.",
+        github_link: "https://github.com/bitster22/kenzie-velopers",
+        vercel_link: ""
+    }
+]
+
+export const repositories_front = [
+    {
+        name: "Kenzie Hub",
+        language: "React",
+        description: "Simulação de repositório de alunos da kenzie, contemplando nível e nome de tecnologias conhecidas.",
+        github_link: "https://github.com/bitster22/kenzie-hub",
+        vercel_link: "https://react-entrega-kenzie-hub-bitster22.vercel.app/"
+    },
+    {
+        name: "Nu Kenzie",
+        language: "React",
+        description: "Gerenciamento de transações financeiras.",
+        github_link: "https://github.com/bitster22/nu-kenzie",
+        vercel_link: "https://react-entrega-s1-template-nu-kenzie-bitster22.vercel.app/"
+    },
+    {
+        name: "Kenzie Burguer",
+        language: "React",
+        description: "Simulação de uma hamburgueria online.",
+        github_link: "https://github.com/bitster22/Kenzie-burguer",
+        vercel_link: "https://react-entrega-s3-template-hamburgueria-bitster22.vercel.app/"
+    },
+    {
+        name: "Git Search",
+        language: "HTML",
+        description: "Pesquisa de repositórios públicos de usuários do github.",
+        github_link: "https://github.com/bitster22/git-search",
+        vercel_link: "https://bitster22.github.io/git-search/"
+    },
+    {
+        name: "Kenzie Empresas",
+        language: "HTML",
+        description: "CRUD de gerenciamento de empresas para RH.",
+        github_link: "https://github.com/bitster22/projeto-empresas",
+        vercel_link: "https://bitster22.github.io/projeto-empresas/"
+    },
+    {
+        name: "Open Music",
+        language: "HTML",
+        description: "Listagem de músicas com filtro de preço e categorias.",
+        github_link: "https://github.com/bitster22/open-music",
+        vercel_link: "https://kenzie-academy-brasil-developers.github.io/open-music-base-bitster22/"
+    },
+]
