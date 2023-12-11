ff133f9ce1a289c72e501ee3ebfb09ca95512c86
bitster22 <57819533+bitster22@users.noreply.github.com>
2023-12-10 21:54:13 -0300

    feat: footer finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import "./index.css";
 import { RoutesMain } from "./routes/routesMain";
@@ -7,6 +8,7 @@ export default function App() {
     <div className=" bg-gray-900 min-h-screen">
       <Header/>
       <RoutesMain/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { MdWavingHand } from "react-icons/md";
+export const Footer = () =>{
+    return(
+        <footer className="bg-gray-950 font-inter sticky top-[100vh]">
+            <div className=" flex items-center gap-4 justify-center p-4">
+                <MdWavingHand color="yellow"/>
+                <span className=" text-gray-200">Obrigado por acessar meu Portfolio!</span>
+            </div>
+        </footer>
+    )
+}
