7a860b5e7ae1f1cd95b5c51ce0cf004c9d77a1cf
bitster22 <57819533+bitster22@users.noreply.github.com>
2023-12-10 21:42:23 -0300

    feat: Página de contatos finalizada

---
+++ src/components/ContactOptionsSection.jsx
@@ -0,0 +1,40 @@
+import { FaWhatsapp, FaLinkedin} from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+
+
+export const ContactOptionsSection = () =>{
+    return(
+        <ul className=" w-2/5 flex flex-col gap-8">
+            <li className=" flex gap-8 items-center">
+                <div className=" bg-green-500 p-6 rounded-md">
+                    <FaWhatsapp size="1.5rem"/>
+                </div>
+                <div className=" flex flex-col gap-3">
+                    <h3 className=" text-lg">Whatsapp</h3>
+                    <p className=" text-sm text-gray-500">Estou disponível, me envie uma mensagem!</p>
+                    <a href="https://wa.me/5521985643871" className=" text-sm hover:text-green-300">Mandar mensagem</a>
+                </div>
+            </li>
+            <li className=" flex gap-8 items-center">
+                <div className=" bg-red-500 p-6 rounded-md">
+                    <MdOutlineEmail size="1.5rem"/>
+                </div>
+                <div className=" flex flex-col gap-3">
+                    <h3 className=" text-lg">Email</h3>
+                    <p className=" text-sm text-gray-500">Me envie um email!</p>
+                    <a href="mailto:alvaro.a.p.r.s@gmail.com" className=" text-sm hover:text-red-300">Enviar Email</a>
+                </div>
+            </li>
+            <li className=" flex gap-8 items-center">
+                <div className=" bg-blue-400 p-6 rounded-md">
+                    <FaLinkedin size="1.5rem"/>
+                </div>
+                <div className=" flex flex-col gap-3">
+                    <h3 className=" text-lg">Linkedin</h3>
+                    <p className=" text-sm text-gray-500">Meu linkedin, me adicione ou mande uma mensagem</p>
+                    <a href="https://www.linkedin.com/in/alvaro-paiva-dos-reis-s%C3%A1-a032991b5/" target="_blank" rel="noreferrer" className=" text-sm hover:text-blue-300">Linkedin</a>
+                </div>
+            </li>
+        </ul>
+    )
+}
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,10 @@
+import { ContactOptionsSection } from "../components/ContactOptionsSection"
+
 export const Contacts = () =>{
     return(
-        <div>contatos</div>
+        <main className=" text-gray-200 max-w-6xl mx-auto flex justify-between items-center p-8 my-10">
+            <h2 className=" w-2/5 text-3xl"><span className=" text-indigo-500">Entre em contato comigo</span> por um dos meios ao lado</h2>
+            <ContactOptionsSection/>
+        </main>
     )
 }
